eecc1537528e7b9851f6314c06ea08da4341242a
Frédéric NOËL <contact@fredericnoel.com>
2020-02-13 16:16:14 +0100

    Initialisation propriété deck avec valeurs par défaut

--- classes/Deck.php
+++ classes/Deck.php
@@ -1,17 +1,16 @@
 <?php
 
-
 class Deck
 {
-    private $deck;
+    public $deck = [[0,0,0], [0,0,0], [0,0,0]];
     private $player1Value = 1;
     private $player2Value = 2;
 
     public function __construct()
     {
-        if (!isset($_SESSION['deck'])) {
-            $this->deck = [[0,0,0], [0,0,0], [0,0,0]];
-            $_SESSION['deck'] = $this->deck;
+        if (!isset($_SESSION['deck']) || $_SESSION['deck'] === false ) {
+            $_SESSION['deck'] = [[0,0,0], [0,0,0], [0,0,0]];
+            $this->deck = $_SESSION['deck'];
         }
     }
 }
