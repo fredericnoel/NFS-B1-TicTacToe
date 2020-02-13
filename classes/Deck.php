c6f5b7316762fcbf89bbb825bbdad0d9fae45cab
Frédéric NOËL <contact@fredericnoel.com>
2020-02-13 16:19:55 +0100

    On stocke le deck par défaut en session s'il n'existe pas

--- classes/Deck.php
+++ classes/Deck.php
@@ -2,15 +2,19 @@
 
 class Deck
 {
-    public $deck = [[0,0,0], [0,0,0], [0,0,0]];
+    private $deck = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
     private $player1Value = 1;
     private $player2Value = 2;
 
     public function __construct()
     {
-        if (!isset($_SESSION['deck']) || $_SESSION['deck'] === false ) {
-            $_SESSION['deck'] = [[0,0,0], [0,0,0], [0,0,0]];
-            $this->deck = $_SESSION['deck'];
+        if (!isset($_SESSION['deck']) || $_SESSION['deck'] === false) {
+            $_SESSION['deck'] = $this->deck;
         }
+    }
+
+    public function displayDeck()
+    {
+
     }
 }
