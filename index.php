b31c0379578656631a9670bb15190970d8e9ee0e
Frédéric NOËL <contact@fredericnoel.com>
2020-02-13 15:47:04 +0100

    Instance pour test

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 
+session_start();
 date_default_timezone_set('Europe/Paris');
 require_once './functions/classAutoLoader.php';
 spl_autoload_register('classAutoLoader');
 
+$choucroute = new Deck();
+var_dump($choucroute);
